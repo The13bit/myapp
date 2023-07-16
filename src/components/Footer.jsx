ee74f6829d6fbd2782fc362d7c213fc2838fcc1d
The13bit <anasfodkar5@gmail.com>
2023-07-17 00:28:32 +0530

    First react app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>Techlight</h1>
+            <p>@all rights reserved</p>
+        </div>
+        <div>
+            <h5>Follow us</h5>
+            <div>
+                <a href="https://www.facebook.com/" target="blank">Facebook</a>
+                <a href="https://github.com/The13bit" target="blank">Github</a>
+                <a href="https://www.facebook.com/" target="blank">Facebook</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
